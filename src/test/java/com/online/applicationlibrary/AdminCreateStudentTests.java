59c3c9f6a4a68558b54e2c8fba369f3f53b8974d
akshatj777 <akshatjain75@gmail.com>
2018-06-13 18:38:45 +0530

    Worked on configuring the base class

---
+++ src/test/java/com/online/applicationlibrary/AdminCreateStudentTests.java
@@ -0,0 +1,9 @@
+package com.online.applicationlibrary;
+
+import com.online.genericlibrary.AdminCreateStudent;
+import com.online.resources.DriverScript;
+
+public class AdminCreateStudentTests extends DriverScript{
+	AdminCreateStudent createstudent=new AdminCreateStudent(driver);
+	
+}
---
+++ src/test/java/com/online/genericlibrary/AdminCreateStudent.java
@@ -0,0 +1,13 @@
+package com.online.genericlibrary;
+
+import org.openqa.selenium.WebDriver;
+
+import com.online.baseClass.BaseClass;
+
+public class AdminCreateStudent extends BaseClass {
+
+	public AdminCreateStudent(WebDriver driver) {
+		super(driver);
+	}
+
+}
